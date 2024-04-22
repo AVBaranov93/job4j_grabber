ec2707461f2f9552ff1be56664cfedda19da4562
AVBaranov93 <avbaranov@internet.ru>
2024-04-22 16:36:24 +1000

    0. Принцип единственной ответственности [#4913]

--- src/main/java/ru/job4j/srp/violations/Product.java
+++ src/main/java/ru/job4j/srp/violations/Product.java
@@ -1,12 +1,13 @@
 package ru.job4j.srp.violations;
 
 public class Product {
-    int amount;
-    Price price;
+    private int amount;
+    private Price price;
 
     public void setPrice(Price newPrice) {
         this.price = newPrice;
     }
+
     public void replenishAmount(int amount) {
         this.amount += amount;
     }
