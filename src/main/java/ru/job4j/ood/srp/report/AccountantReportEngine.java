126c0a34dc255ab9cd34f0a39cee76bcceee87e6
andrey <avbaranov@internet.ru>
2024-04-24 14:56:12 +1000

    1. Отчеты. [#850]

--- src/main/java/ru/job4j/ood/srp/report/AccountantReportEngine.java
+++ src/main/java/ru/job4j/ood/srp/report/AccountantReportEngine.java
@@ -1,14 +1,13 @@
 package ru.job4j.ood.srp.report;
 
-import ru.job4j.ood.srp.currency.Currency;
 import ru.job4j.ood.srp.currency.CurrencyConverter;
+import ru.job4j.ood.srp.currency.InMemoryCurrencyConverter;
 import ru.job4j.ood.srp.formatter.DateTimeParser;
 import ru.job4j.ood.srp.model.Employee;
 import ru.job4j.ood.srp.store.Store;
-
 import java.util.Calendar;
-import java.util.List;
 import java.util.function.Predicate;
+import ru.job4j.ood.srp.currency.Currency;
 
 public class AccountantReportEngine implements Report {
     private final Store store;
@@ -31,7 +30,6 @@ public class AccountantReportEngine implements Report {
         StringBuilder text = new StringBuilder();
         text.append("Name; Hired; Fired; Salary;")
                 .append(System.lineSeparator());
-        List<Employee> employees = store.findBy(filter);
         for (Employee employee : store.findBy(filter)) {
             text.append(employee.getName()).append(" ")
                     .append(dateTimeParser.parse(employee.getHired())).append(" ")
@@ -40,9 +38,5 @@ public class AccountantReportEngine implements Report {
                     .append(System.lineSeparator());
         }
         return text.toString();
-    }
-
-    public double convert(Currency source, double salary, Currency target) {
-        return converter.convert(source, salary, target);
     }
 }
